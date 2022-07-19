b961d8eef52daa462f223e71807e68f493fc6269
Sonali Mali <sonali.mali@olacabs.com>
2022-07-19 15:16:41 +0530

    chore:import the component and register it with components tag and used it

--- first_vue_project/src/App.vue
+++ first_vue_project/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App created by Sonali Mali"/>
+  <Home msg="ok"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Home
   }
 }
 </script>

3aa0d0b5f5f6ed60764d6b5a8b35e6a7e96fb3c5
Sonali Mali <sonali.mali@olacabs.com>
2022-07-20 14:01:33 +0530

    feat:created input value component for accessig the iput values from input box

---
+++ vue_events/src/components/InputValues.vue
@@ -0,0 +1,31 @@
+<template>
+    <hr>
+    <h1>Get the Input value</h1>
+    <input type="text" placeholder="Enter your email" v-model="email">
+    <br><br>
+    <input type="password" placeholder="Enter your password" v-model="password">
+    <br><br>
+    <button type="button" v-on:click="getData()">Get Data</button>
+    <p>{{ email }}{{ password }} </p>
+</template>
+<script>
+export default {
+    name: "InputValues",
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        getData() {
+            console.log("Values : ", this.email, this.password)
+        }
+    }
+}
+</script>
+<style>
+hr {
+    border: 1px solid orangered;
+}
+</style>

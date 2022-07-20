f2e14232d5c3166964ccff68e36bacade4880246
Sonali Mali <sonali.mali@olacabs.com>
2022-07-20 12:15:01 +0530

    created data_binding component, register it and practice done for two way binding

--- vue_events/src/App.vue
+++ vue_events/src/App.vue
@@ -2,27 +2,30 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <Home/>
+  <DataBinding/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import Home from './components/Home.vue'
+import DataBinding from './components/data_binding.vue'
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    Home
+    Home,
+    DataBinding
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>

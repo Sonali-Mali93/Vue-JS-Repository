cf57509e6069511a174dbd3850c6acaaba54863d
Sonali Mali <sonali.mali@olacabs.com>
2022-07-20 11:35:09 +0530

    vue js events practice

---
+++ vue_events/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1 v-on:mousemove="mouse_click()">Home Componet</h1>
+    <button v-on:click="getDate()">Click Here to increase count</button>
+    <button v-on:dblclick="test()">CLICK ME</button>
+    <h1 v-on:focusout="mouse_click()">CLICK ME</h1>
+    <p>{{ count }}</p>
+    <p></p>
+</template>
+<script>
+export default {
+    name: "HomeComponent",
+    data() {
+        return {
+            count : 0,
+        }
+    },
+    methods: {
+        getDate() {
+            this.count = this.count - 1
+        },
+        test() {
+            alert("Dobule click event is called")
+        },
+        mouse_click() {
+            console.log("ok")
+        }
+    }
+}
+</script>
